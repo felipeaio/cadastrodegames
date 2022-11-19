be29162b3d712b04504ad03d53bea1c879457819
Felipe Aio <felipeaio@hotmail.com>
2022-11-19 15:34:06 -0300

    commit

--- src/main/java/br/com/felipeaio/CadastrodeGames/config/WebConfig.java
+++ src/main/java/br/com/felipeaio/CadastrodeGames/config/WebConfig.java
@@ -1,23 +1,34 @@
 package br.com.felipeaio.CadastrodeGames.config;
 
+import br.com.felipeaio.CadastrodeGames.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    //..add a media type
+    private static final MediaType MEDIA_TYPE_YAML = MediaType.valueOf("application/x-yaml");
+
+    //..add a interceptor
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
         configurer.favorParameter(true)
-                .parameterName("mediaType")
-                .ignoreAcceptHeader(true)
+                .ignoreAcceptHeader(false) //.. remove parameterName method and don't ignore Accept Header
                 .useRegisteredExtensionsOnly(false)
                 .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML)
-                .mediaType("json", MediaType.APPLICATION_JSON);
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("x-yaml", MEDIA_TYPE_YAML); //..adding YAML support
     }
 }
--- src/main/java/br/com/felipeaio/CadastrodeGames/serialization/converter/YamlJackson2HttpMessageConverter.java
+++ src/main/java/br/com/felipeaio/CadastrodeGames/serialization/converter/YamlJackson2HttpMessageConverter.java
@@ -1,33 +1,11 @@
 package br.com.felipeaio.CadastrodeGames.serialization.converter;
 
-import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 import org.springframework.http.MediaType;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import java.util.List;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    //..add a media type
-    private static final MediaType MEDIA_TYPE_YAML = MediaType.valueOf("application/x-yaml");
-
-    //..add a interceptor
-    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
-        converters.add(new YamlJackson2HttpMessageConverter());
-    }
-
-    @Override
-    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
-        configurer.favorParameter(true)
-                .ignoreAcceptHeader(false) //.. remove parameterName method and don't ignore Accept Header
-                .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML)
-                .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("x-yaml", MEDIA_TYPE_YAML); //..adding YAML support
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+public class YamlJackson2HttpMessageConverter extends
+        AbstractJackson2HttpMessageConverter {
+    public YamlJackson2HttpMessageConverter(){
+        super(new YAMLMapper(), MediaType.parseMediaType("application/x-yaml"));
     }
 }
